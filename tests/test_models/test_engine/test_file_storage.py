8f76b4ed74798507233093c4db70a01e3be3d3f3
modek2013 <2013kulbaibe@gmail.com>
2026-09-12 08:13:28 +0100

    Fix TypeError assertion message compatibility in file storage tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -47,8 +47,8 @@ class TestFileStorage(unittest.TestCase):
         self.resetStorage()
         with self.assertRaises(TypeError) as e:
             b = FileStorage(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-        msg = "object() takes no parameters"
-        self.assertEqual(str(e.exception), msg)
+        msg = ["object() takes no parameters", "FileStorage() takes no arguments"]
+        self.assertIn(str(e.exception), msg)
 
     def test_5_attributes(self):
         """Tests class attributes."""
